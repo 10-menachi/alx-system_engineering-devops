0087365393d30586b89dfe9f1612493582bdb651
10-menachi <bobitlmr188221@spu.ac.ke>
2023-11-30 08:53:02 +0300

    Fix bug in code

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # The regular expression must match hbtn whereby
 # the letter t is repeated at least 2 times and at most 5 times
-puts ARGV[0].scan(/hbt{2,5}n/).join
+puts ARGV[0].scan(/hb{2,5}tn/).join
