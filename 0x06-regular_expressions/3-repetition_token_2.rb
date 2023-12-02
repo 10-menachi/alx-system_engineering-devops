255946899362f58b969454112782c5cda8632746
10-menachi <bobitlmr188221@spu.ac.ke>
2023-12-02 09:17:55 +0300

    Fix bug in code

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# The regular expression must match School
+# The regular expression must match hbtn whereby the letter t can be present zero or more times.
 puts ARGV[0].scan(/hbt+n/).join
